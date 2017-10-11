f7aede9425454b1fb869a4de6d3a0b83b352c189
kmelve <knut@netliferesearch.com>
2017-10-11 11:06:55 +0200

    use correct paths in install

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 DIR=/usr/share/nginx/app
-
+ls /config
 # check if we need to install Craft
 if [ "$(ls -A $DIR)" ]; then
      echo "Can't install Craft because $DIR is not empty"
 else
     composer create-project craftcms/craft $DIR -s beta
     cp /.env $DIR
-    cp /usr/share/nginx/config/general.php $DIR/config/general.php
-    cp /usr/share/nginx/config/memcache.php $DIR/config/memcache.php
+    cp /config/general.php $DIR/config/general.php
+    cp /config/memcache.php $DIR/config/memcache.php
+
 fi
 
 # Start nginx
