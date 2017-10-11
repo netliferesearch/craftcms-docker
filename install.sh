0935e7e7a8f0fc49a455d231dc1ca7575c3aa6e9
kmelve <knut@netliferesearch.com>
2017-10-11 10:10:17 +0200

    fix paths

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ if [ "$(ls -A $DIR)" ]; then
 else
     composer create-project craftcms/craft $DIR -s beta
     cp /.env $DIR
-    cp ./config/general.php $DIR/config/general.php
-    cp ./config/memcache.php $DIR/config/memcache.php
+    cp /usr/share/nginx/config/general.php $DIR/config/general.php
+    cp /usr/share/nginx/config/memcache.php $DIR/config/memcache.php
 fi
 
 # Start nginx
