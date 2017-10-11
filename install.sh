768bbaee19838388567c07eab2ff641d9f12d67f
kmelve <knut@netliferesearch.com>
2017-10-11 16:24:28 +0200

    update install script

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ ls /config
 if [ "$(ls -A $DIR)" ]; then
      echo "Can't install Craft because $DIR is not empty"
 else
+    echo "INSTALLING CRAFT"
     composer create-project craftcms/craft $DIR -s beta
     cp /.env $DIR
     cp /config/general.php $DIR/config/general.php
     cp /config/memcache.php $DIR/config/memcache.php
-
+    composer require heroku/heroku-buildpack-php --dev
 fi
 
 # Start nginx
