c5f0938886cd988103b2205b0122a83c448cef06
Nils Norman Haukås <nils@thunki.com>
2017-10-13 13:15:59 +0200

    call composer install after composer create-project

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ if [ "$(ls -A $DIR)" ]; then
 else
     echo "INSTALLING CRAFT"
     composer create-project craftcms/craft $DIR -s beta
+    composer install -d $DIR
+    # We move the files inside the docker container into mounted folder so that
+    # they'll re-emerge on the user's host pc from within the bowels of the container.
     cp /.env $DIR
     cp /config/general.php $DIR/config/general.php
     cp /config/memcache.php $DIR/config/memcache.php
